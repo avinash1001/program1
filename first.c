7c633d6326eedf766f8dcfd90b78430cd2c5f623
avinash1001 <35587432+avinash1001@users.noreply.github.com>
2019-04-02 12:01:19 +0530

    Update first.c

--- first.c
+++ first.c
@@ -1,5 +1,19 @@
-#include<stdio.h>
-void main()
+#include <stdio.h>
+ 
+int main()
 {
-	printf("hello world!");
+  int n, c, k;
+ 
+  printf("Enter number of rows\n");
+  scanf("%d", &n);
+ 
+  for (c = 1; c <= n; c++)
+  {
+    for(k = 1; k <= c; k++)
+      printf("*");
+ 
+    printf("\n");
+  }
+ 
+  return 0;
 }
